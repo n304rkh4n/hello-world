89640be179004528540d509c8cd9eec6bb8f0cda
n304rkh4n <81456718+n304rkh4n@users.noreply.github.com>
2021-03-27 01:47:26 -0300

    Add files via upload

---
+++ datatest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 02:37:02 2021
+
+@author: Gui
+"""
+#função read.csv abrindo tabela de magias circl0
+#usando pandas como biblioteca pra chamar a funcao read.csv
+
+import pandas as pd
+datacsv = pd.read_csv( 'data.csv' )
